698d4cd89e187b290cfbe409e44e662ab4085d50
Alfi Filsafalasafi <53686783+Alfi-Filsafalasafi@users.noreply.github.com>
2024-11-12 20:27:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
-from flask import Flask, render_template, request
-from transformers import pipeline
+from flask import Flask
 
-# Inisialisasi aplikasi Flask
 app = Flask(__name__)
 
-# Inisialisasi model parafrase
-paraphraser = pipeline("text2text-generation", model="t5-base")
-
-# Halaman utama dengan formulir input teks
-@app.route("/", methods=["GET", "POST"])
+@app.route('/')
 def home():
-    paraphrased_text = ""
-    if request.method == "POST":
-        original_text = request.form["original_text"]
-        result = paraphraser(original_text, max_length=50, num_return_sequences=1)
-        paraphrased_text = result[0]['generated_text']
-    return render_template("templates/index.html", paraphrased_text=paraphrased_text)
+    return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
